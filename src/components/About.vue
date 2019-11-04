3e652fe105fa13c88bbb1510d3b00cba8c6b19ca
Chalizi <Chalizi@qq.com>
2019-11-04 23:30:21 +0800

    customers first commit

---
+++ src/components/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="about" class="container">
+    <h1 class="page-header">希望能够提供给大家更有用的课程</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about'
+}
+</script>
+<style scoped>
+</style>
